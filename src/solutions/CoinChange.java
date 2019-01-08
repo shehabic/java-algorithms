f0eafb27d1943f82efd047a897ce068dae3e9848
shehabic <mohamed.osman@deliveryhero.com>
2019-01-08 01:07:00 +0100

    Adding even more algorithms

--- src/solutions/CoinChange.java
+++ src/solutions/CoinChange.java
@@ -8,17 +8,16 @@ public class CoinChange {
         return makeChange(coins, amount, new HashMap<>());
     }
 
-    private int makeChange(int[] coins, int rem, HashMap<Integer, Integer> mem) {
-        if (rem < 0) return -1;
-        if (rem == 0) return 0;
-        if (mem.containsKey(rem)) return mem.get(rem);
+    private int makeChange(int[] coins, int amount, HashMap<Integer, Integer> memo) {
+        if (amount < 0) return -1;
+        if (amount == 0) return 0;
+        if (memo.containsKey(amount)) return memo.get(amount);
         int best = Integer.MAX_VALUE;
-        for (int coin: coins) {
-            int res = makeChange(coins, rem - coin, mem);
-            if (res > -1 && res < best) best = 1 + res;
+        for (int coin : coins) {
+            int num = makeChange(coins, amount - coin, memo);
+            if (num != -1 && best > num) best = num + 1;
         }
-        mem.put(rem, best == Integer.MAX_VALUE ? -1 : best);
-
-        return mem.get(rem);
+        memo.put(amount, best == Integer.MAX_VALUE ? -1 : best);
+        return memo.get(amount);
     }
 }
