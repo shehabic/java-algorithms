2357bc8357bb2258a24c7cc62eea79fcaf1953d4
shehabic <mohamed.osman@deliveryhero.com>
2019-01-24 23:55:24 +0100

    Adding even more algorithms

--- src/solutions/MaximumSumSubArrays.java
+++ src/solutions/MaximumSumSubArrays.java
@@ -9,13 +9,14 @@ public class MaximumSumSubArrays {
     public static void test() {
         MaximumSumSubArrays sut = new MaximumSumSubArrays();
         int[][] samples = new int[][]{
+            {3, 2, 1, 10, 5, 1, 2, 16, 9, 10, 4, 5, 7},
             {4, 5, 10, 6, 11, 17, 4, 11, 1, 3},
             {7, 13, 20, 19, 19, 2, 10, 1, 1, 19},
             {1, 3, 4, 5, 6, 7, 10, 2, 3, 4, 7, 9, 3, 5, 6, 7},
             {2, 3, 0, 1, 2, 7, 0, 4, 5},
             {1, 2, 1, 2, 6, 7, 5, 1}
         };
-        int[] k = new int[]{1, 3, 3, 2, 2};
+        int[] k = new int[]{3, 1, 3, 3, 2, 2};
         int i = 0;
         for (int[] sample : samples) {
 
@@ -52,7 +53,9 @@ public class MaximumSumSubArrays {
             if (sum[i] >= sum[best]) best = i;
             right[i] = best;
         }
-
+        System.out.println("Sum: " + Printing.printArray(sum));
+        System.out.println("Left: " + Printing.printArray(left));
+        System.out.println("Right: " + Printing.printArray(right));
         int[] res = new int[]{-1, -1, -1};
 
         for (int i = k; i <= n - 2 * k; i++) {
