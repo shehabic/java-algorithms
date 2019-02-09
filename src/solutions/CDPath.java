994b5d717f262733fff713a97bfc9c4350654752
shehabic <mohamed.osman@deliveryhero.com>
2019-02-09 01:33:05 +0000

    Adding even more algorithms

--- src/solutions/CDPath.java
+++ src/solutions/CDPath.java
@@ -7,26 +7,28 @@ import java.util.List;
 
 public class CDPath {
     public static String path(String currentPath, String newPath) {
-            String path = newPath.startsWith("/") ? newPath : currentPath + "/" + newPath;
-            Deque<String> stack = new LinkedList<>();
-            String[] parts = path.split("/");
-            List<String> result = new ArrayList<>();
-            String output = "";
-            for (String s: parts) {
-                if (s.equals(".") || s.equals("")) continue;
-                if (s.equals("..")) {
-                    if (!stack.isEmpty()) stack.pop();
-                } else {
-                    stack.push(s);
-                }
+        String path = newPath.startsWith("/") ? newPath : currentPath + "/" + newPath;
+        Deque<String> stack = new LinkedList<>();
+        String[] parts = path.split("/");
+        List<String> result = new ArrayList<>();
+        String output;
+        for (String s : parts) {
+            if (s.equals(".") || s.equals("")) continue;
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(s);
             }
-            while (!stack.isEmpty()) {
-                result.add(stack.pop());
-            }
-            for (int i = result.size() - 1; i >= 0; i--) {
-                output += "/" + result.get(i);
-            }
-            return output.length() == 0 ? "/" : output;
+        }
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        StringBuilder outputBuilder = new StringBuilder();
+        for (int i = result.size() - 1; i >= 0; i--) {
+            outputBuilder.append("/").append(result.get(i));
+        }
+        output = outputBuilder.toString();
+        return output.length() == 0 ? "/" : output;
     }
 
     public static void test() {
