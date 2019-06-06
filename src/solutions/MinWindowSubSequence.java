e7926462d3693fa3cd27a70484882603e022d8af
shehabic <mohamed.osman@deliveryhero.com>
2019-06-07 01:13:46 +0200

    Updating robot room cleaner

--- src/solutions/MinWindowSubSequence.java
+++ src/solutions/MinWindowSubSequence.java
@@ -3,18 +3,18 @@ package solutions;
 public class MinWindowSubSequence {
     public String minWindow(String S, String T) {
         String window = "";
-        int tIndex = 0, min = S.length() + 1;
+        int j = 0, min = S.length() + 1;
         for (int i = 0; i < S.length(); i++) {
-            if (S.charAt(i) == T.charAt(tIndex)) {
-                tIndex++;
-                if (tIndex == T.length()) {
+            if (S.charAt(i) == T.charAt(j)) {
+                j++;
+                if (j == T.length()) {
                     int end = i + 1;
-                    tIndex--;
-                    while (tIndex >= 0) {
-                        if (S.charAt(i) == T.charAt(tIndex)) tIndex--;
+                    j--;
+                    while (j >= 0) {
+                        if (S.charAt(i) == T.charAt(j)) j--;
                         i--;
                     }
-                    tIndex++;
+                    j++;
                     i++;
                     if (end - i < min) {
                         min = end - i;
